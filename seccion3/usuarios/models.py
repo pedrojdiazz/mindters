b757ab25ef4f92c3a3e9bde7f4cdff0a37bb9ced
Pedro Diaz <petrosdiaz@gmail.com>
2024-05-21 23:16:41 -0300

    a

--- seccion3/usuarios/models.py
+++ seccion3/usuarios/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Usuario(models.Model):
     nombre = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
     
